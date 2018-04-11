462694bff0bcfabbde606c2f74ad8b6390e5e3d4
vineet-kld <37562287+vineet-kld@users.noreply.github.com>
2018-04-11 23:54:53 +0530

    Working Properly but Limited to 3 Students only.

--- the_sleeping_teaching_assiatant.c
+++ the_sleeping_teaching_assiatant.c
@@ -1,91 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
-#include<semaphore.h>
-#include<time.h>
-#include<unistd.h>
+void* Programming(void* ID);
+void* Assisting();
+
+sem_t sem_STU;
+sem_t sem_TA;
+
+pthread_mutex_t lock;
 
 int chair[3];
-int count=0;
-int Index=0;
-int front=0;
-int rear=0;
 
-sem_t Student_sem;
-sem_t TA_sem;
-
-pthread_mutex_t Chair_Access;
-
-void *Student(void* ID)
-{
-while(1)
-{
-sleep(rand()%5+1);
-if(count<3)
-{
-chair[front]=(long)ID;
-if(count==0)
-sem_post(&TA_sem);
-
-pthread_mutex_lock(&Chair_Access);
-
-int pt = (count+Index) % 3;
-count++;
-
-printf("Student Occupy Chair. Left Chair : %d \n",3-count);
-
-pthread_mutex_unlock(&Chair_Access);
-}
-
-else
-printf("No seat is left. Student will come afterward.\n");
-}
-
-}
-
-void *Teacher()
-{
-while(1)
-{
-sem_wait(&TA_sem);
-printf("Teacher Assiantant is awakened by a Student\n");
-
-pthread_mutex_lock(&Chair_Access);
-Index=(Index+1)%3;
-pthread_mutex_unlock(&Chair_Access);
-
-printf("Teacher Assiantant is Teaching Student\n");
-
-sleep(rand()%5+1);
-
-sem_post(&TA_sem);
-}
-}
+int front=0,rear=0,waiting=0,sleeping=0;
 
 int main()
 {
-pthread_t student[3];
+int n=3;
+  
+pthread_t Student[n];
 pthread_t TA;
 
-srand(time(NULL));
+sem_init(&sem_STU,0,0);
+sem_init(&sem_TA,0,1);
 
-sem_init(&Student_sem,0,0);
-sem_init(&TA_sem,0,1);
+pthread_mutex_init(&lock,NULL);
 
-pthread_mutex_init(&Chair_Access,NULL);
+pthread_create(&TA,NULL,Assisting,NULL);
+for(int i=0;i<n;i++)
+pthread_create(&Student[i],NULL,Programming,(void*)(long)i);
 
-int i;
-
-for(i=0;i<3;i++)
-pthread_create(&student[i],NULL,Student,(void*)(long)i);
-
-pthread_create(&TA,NULL,Teacher,NULL);
-
-for(i=0;i<3;i++)
-pthread_join(student[i],NULL);
+for(int i=0;i<n;i++)
+pthread_join(Student[i],NULL);
 
 pthread_join(TA,NULL);
 
 return 0;
+}
+
+void* Assisting()
+{
+printf("--------------------------------------------------------\n");
+while(1)
+{
+	if(waiting>0)
+	{
+	sleeping=0;
+	
+	sem_wait(&sem_STU);
+	
+	pthread_mutex_lock(&lock);
+	
+	printf("TA started assisting Student %d\n",chair[rear]);
+	printf("           Students Wating = %d\n",(waiting-1));
+	printf("--------------------------------------------------------\n");
+
+	chair[rear]=0;
+	waiting--;
+	rear=(rear+1)%3;
+	
+	sleep(rand()%5);
+	
+	pthread_mutex_unlock( &lock );
+	sem_post( &sem_TA );
+	}
+	else 
+	{
+	if(sleeping==0) 
+	{	
+	printf("No students are waiting.So TA is Sleeping...\n");
+	sleeping=1;
+	}
+	}
+}
+}
+
+void* Programming(void * ID) 
+{
+while(1)
+{
+	if(chair[0]==(long)ID+1||chair[1]==(long)ID+1||chair[2]==(long)ID+1)
+	continue;
+	
+	printf("Student %ld programming.\n",(long)ID+1);
+	sleep(rand()%5);
+
+	pthread_mutex_lock(&lock);
+
+	if(waiting<3)
+	{
+	chair[front]=(long)ID+1;
+	waiting++;
+
+	printf("--------------------------------------------------------\n");
+	printf("Student %ld has doubt.\n",(long)ID+1);
+	if(waiting>1)
+	printf("Student %ld takes the seat in the Hallway.\n",(long)ID+1);
+	else
+	printf("Student %ld takes the seat in the Cabin.\n",(long)ID+1);
+	front=(front+1)%3;
+
+	pthread_mutex_unlock(&lock);
+
+	sem_post(&sem_STU);
+	sem_wait(&sem_TA);
+	}
+	else 
+	{
+	pthread_mutex_unlock(&lock);
+	printf("Hallway is full.\n");
+	printf("Student %ld will come back later...\n",(long)ID);
+	}
+}
 }
