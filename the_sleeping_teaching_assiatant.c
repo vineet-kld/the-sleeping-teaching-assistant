cfea6f92e7d572c771037995960e1a8bd6245fbe
vineet-kld <37562287+vineet-kld@users.noreply.github.com>
2018-04-12 00:23:58 +0530

    Final Code With Comments

--- the_sleeping_teaching_assiatant.c
+++ the_sleeping_teaching_assiatant.c
@@ -5,16 +5,25 @@
 #include <semaphore.h>
 
 void* Programming(void* ID);
+// Student doing its Programming.
+
 void* Assisting();
+// TA Assisting in cae Student gets doubt.
 
 sem_t sem_STU;
 sem_t sem_TA;
+// TA Semaphore to make TA sleep.
 
 pthread_mutex_t lock;
+// Mutex lock to lock the Shared Variables.
 
 int chair[3];
 
 int front=0,rear=0,waiting=0,sleeping=0;
+//   front : This is for seating position.
+//    rear : This is for Teaching status(position).
+// waiting : This is to check that How many student are waiting.
+// sleeping: This is to keep track of Teaching Assistant's sleep.
 
 int main()
 {
@@ -22,6 +31,7 @@ int n=0,t;
 
 do
 {
+// Office Time
 printf("Enter Current Time in 24 hr Format...\n");
 scanf("%d",&t);
 
@@ -30,22 +40,28 @@ if(t<8||t>20)
 printf("It is Not an Office Hour.TA is Sleeping... Visit Later.\n");
 continue;
 }
+// Number of Student.
 printf("Enter the number of Students...\n");
 scanf("%d",&n);
 }while((n<1)||(t<8)||(t>20));
 
+// Defining Threads 
 pthread_t Student[n];
 pthread_t TA;
 
+// Initilizing semaphore
 sem_init(&sem_STU,0,0);
 sem_init(&sem_TA,0,1);
 
+// Initilizing Mutex lock
 pthread_mutex_init(&lock,NULL);
 
+// Thread Creation
 pthread_create(&TA,NULL,Assisting,NULL);
 for(int i=0;i<n;i++)
 pthread_create(&Student[i],NULL,Programming,(void*)(long)i);
 
+// Thread Joining
 for(int i=0;i<n;i++)
 pthread_join(Student[i],NULL);
 
@@ -66,7 +82,7 @@ while(1)
 	sem_wait(&sem_STU);
 	
 	pthread_mutex_lock(&lock);
-	
+// Started Assisting the Student.
 	printf("TA started assisting Student %d\n",chair[rear]);
 	printf("           Students Wating = %d\n",(waiting-1));
 	printf("--------------------------------------------------------\n");
@@ -84,6 +100,7 @@ while(1)
 	{
 	if(sleeping==0) 
 	{	
+// If no student is present in Hallway. TA will take Nap.
 	printf("No students are waiting.So TA is Sleeping...\n");
 	sleeping=1;
 	}
@@ -95,11 +112,13 @@ void* Programming(void * ID)
 {
 while(1)
 {
+// If Student is waiting Let him wait.
 	if(chair[0]==(long)ID+1||chair[1]==(long)ID+1||chair[2]==(long)ID+1)
 	continue;
 	
+// Student Start Programming.
 	printf("Student %ld programming.\n",(long)ID+1);
-	sleep(rand()%5);
+	sleep(rand()%5); // Random Sleep(Programming) Time
 
 	pthread_mutex_lock(&lock);
 
@@ -108,6 +127,7 @@ while(1)
 	chair[front]=(long)ID+1;
 	waiting++;
 
+// Student takes a seat in the Cabin & Hallway Accordingly.
 	printf("--------------------------------------------------------\n");
 	printf("Student %ld has doubt.\n",(long)ID+1);
 	if(waiting>1)
@@ -119,10 +139,12 @@ while(1)
 	pthread_mutex_unlock(&lock);
 
 	sem_post(&sem_STU);
-	sem_wait(&sem_TA);
+// Awake TA if Sleeping
+	sem_wait(&sem_TA); 
 	}
 	else 
 	{
+// When Hallway's Chair are full Student will be send back.
 	pthread_mutex_unlock(&lock);
 	printf("Hallway is full.\n");
 	printf("Student %ld will come back later...\n",(long)ID);
